c97fc32cdf8a33797dc7db425636b02533e542a9
Alli Campbelll <allison.campbell@ou.edu>
2017-05-08 15:30:05 -0500

    express app can connect to angular app to get comics

--- src/app.js
+++ src/app.js
@@ -3,14 +3,24 @@
 var express = require('express');
 //create instance of restify server 
 var app = express();
+//make a router
+var router = express.Router();
 
 //serve static files in public directory, use a slash as first parameter
 app.use('/', express.static('public'));
 
+
+
 //First Route for API to return Json object
-app.get('/api/comic' , function( req, res){
+router.get('/api/comic' , function( req, res){
 	res.json({comic:[]});
 })
+
+
+//TODO: Add post route, put route, delete route
+
+//mount router to app
+app.use('/api', router);
 
 
 
